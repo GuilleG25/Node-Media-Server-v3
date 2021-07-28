15d395a9705ffb3e2c5f8931d97d0776ab84f063
Your Name <you@example.com>
2021-07-28 18:01:40 +0000

    first commit

---
+++ src/node_core_ctx.js
@@ -0,0 +1,19 @@
+//
+//  Created by Mingliang Chen on 18/3/2.
+//  illuspas[a]gmail.com
+//  Copyright (c) 2018 Nodemedia. All rights reserved.
+//
+const EventEmitter = require('events');
+const EventCheck = require('./node_event_check');
+
+let sessions = new Map();
+let publishers = new Map();
+let idlePlayers = new Set();
+let nodeEvent = new EventEmitter();
+let nodeCheck = EventCheck;
+let stat = {
+  inbytes: 0,
+  outbytes: 0,
+  accepted: 0
+};
+module.exports = { sessions, publishers, idlePlayers, nodeEvent, nodeCheck, stat };
